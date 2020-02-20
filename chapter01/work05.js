e8ddeddae85feefd3b8502545f6cf05291799412
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-02-20 04:33:50 +0000

    fix chapter01 for master

--- chapter01/work05.js
+++ chapter01/work05.js
@@ -1,17 +1,4 @@
 var rank = 1;
 
 // ここから下に判定表に基づいたプログラムを作成します
-switch( rank ) {
-    case 1:
-        console.log('おめでとう。優勝です');
-        break;
-    case 2:
-        console.log('もう少しでしたね');
-        break;
-    case 3:
-        console.log('あと少し頑張ろう');
-        break;
-    default:
-        console.log('残念でした');
-        break;
-}
+
--- chapter01/work06.js
+++ chapter01/work06.js
@@ -1,14 +1,4 @@
 var character = '魔法使い';
 
 // ここから下に判定表に基づいたプログラムを作成します
-switch ( character ) {
-    case '勇者':
-        console.log("勇気をもって戦います");
-        break;
-    case '魔法使い':
-        console.log("魔法が得意です");
-        break;
-    case '戦士':
-        console.log("戦いは強いです");
-        break;
-}
+
