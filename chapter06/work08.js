6622f23eef34c8dcbd3c6d936b217701e13bf50b
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2020-08-06 10:22:28 +0000

    fix chapter06 added this

--- chapter06/work08.js
+++ chapter06/work08.js
@@ -3,14 +3,13 @@ function Calc(num1, num2) {
     this.num1 = num1;
     this.num2 = num2;
     this.add = function() {
-        return this.num1 + num2;
+        return this.num1 + this.num2;
     };
     this.multiply = function() {
-        return this.num1 * num2;
+        return this.num1 * this.num2;
     };
 }
 
 // コンストラクタ関数を利用してオブジェクトを作成
 var calc1 = new Calc(3,5);
 // メソッドを呼び出してください
-
